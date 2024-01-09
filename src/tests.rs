8d619ddb859641e9d3f2d49367b27796cad55aea
parazyd <parazyd@dyne.org>
2024-01-09 16:28:58 +0100

    Update copyright year

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,8 @@
 /* This file is part of Kyber-KEM (https://github.com/parazyd/kyber-kem)
- * * Copyright (C) 2023 Dyne.org foundation * * This program is free software: you can redistribute it and/or modify
+ *
+ * Copyright (C) 2023-2024 Dyne.org foundation
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
